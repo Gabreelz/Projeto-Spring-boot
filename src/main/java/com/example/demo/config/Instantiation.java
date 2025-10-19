54decd9d53c2539cb00e488092a04f6a60541186
Gabreelz <gabriel_teixeira30@hotmail.com>
2025-10-19 17:52:01 -0300

    Adicionando o Post e PostRepository referenciando com o User

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.example.demo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.Repository.PostRepository;
 import com.example.demo.Repository.UserRepository;
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 
 @Configuration
@@ -14,16 +18,28 @@ public class Instantiation implements CommandLineRunner {
     
     @Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... arg0) throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
-		User maria = new User(null, "Peter Parker", "clarim.diario@gmail.com");
-		User alex = new User(null, "Logan", "x@gmail.com");
-		User bob = new User(null, "Scott", "x@gmail.com");
+		User peter = new User(null, "Peter Parker", "clarim.diario@gmail.com");
+		User logan = new User(null, "Logan", "x@gmail.com");
+		User scott = new User(null, "Scott", "x@gmail.com");
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("19/10/2025"), "Amazing Fantasy", "Cabeça de teia", peter);
+		Post post2 = new Post(null, sdf.parse("21/10/2025"), "Days of Future Past", "Wolverine", logan);
+
+		
+		userRepository.saveAll(Arrays.asList(peter, logan, scott));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
